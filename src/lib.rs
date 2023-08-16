b1d6a6b0093a47a464867e5f41086925e3c58899
137-Trimethylxanthin <xxv.many@gmail.com>
2023-08-16 21:12:56 +0200

    i think it works now

--- src/lib.rs
+++ src/lib.rs
@@ -4,70 +4,73 @@ use base64::{Engine as _, engine::{self, general_purpose}, alphabet};
 
 const DEFAULT_KEY:&str = "UGlzc2JvbWJlcjppbm5lbjMxNDE1OWdOdEZpRmdsQ05pTmVqRkZSZ2ZHRHZKSXVUQ3ZFTmJSZHVuR25F";
 
-pub fn encrypt(variable: &str, key: Option<&str>) -> String {
-    let mut result = String::new();
-    let mut key_chars = key.unwrap_or(DEFAULT_KEY).chars().cycle();
-    for c in variable.chars() {
-        let key_char = key_chars.next().unwrap();
-        let key_char_num = key_char.to_digit(36).unwrap();
-        let c_num = c.to_digit(36).unwrap();
-        let new_char_num = (c_num + key_char_num) % 36;
-        let new_char = std::char::from_digit(new_char_num, 36).unwrap();
-        result.push(new_char);
+pub fn encrypt(variable:&str, key:Option<&str>) -> String {
+    let key = key.unwrap_or(DEFAULT_KEY);
+    let mut encrypted = String::new();
+    for (i, c) in variable.chars().enumerate() {
+        let key_char = key.chars().nth(i % key.len()).unwrap();
+        let encrypted_char = (c as u8 ^ key_char as u8) as char;
+        encrypted.push(encrypted_char);
     }
-    result
+    base64_encode(&encrypted)
 }
 
-
-pub fn decrypt(variable: &str, key: Option<&str>) -> String {
-    let mut result = String::new();
-    let mut key_chars = key.unwrap_or(DEFAULT_KEY).chars().cycle();
-    for c in variable.chars() {
-        let key_char = key_chars.next().unwrap();
-        let key_char_num = key_char.to_digit(36).unwrap();
-        let c_num = c.to_digit(36).unwrap();
-        let new_char_num = (c_num + 36 - key_char_num) % 36;
-        let new_char = std::char::from_digit(new_char_num, 36).unwrap();
-        result.push(new_char);
+pub fn decrypt(variable:&str, key:Option<&str>) -> String {
+    let key = key.unwrap_or(DEFAULT_KEY);
+    let encrypted = base64_decode(variable);
+    let mut decrypted = String::new();
+    for (i, c) in encrypted.chars().enumerate() {
+        let key_char = key.chars().nth(i % key.len()).unwrap();
+        let decrypted_char = (c as u8 ^ key_char as u8) as char;
+        decrypted.push(decrypted_char);
     }
-    result
+    decrypted
 }
 
 pub fn base64_encode(variable:&str) -> String {
-    general_purpose::STANDARD.encode(variable)
+    general_purpose::STANDARD_NO_PAD.encode(variable)
 }
 
 pub fn base64_decode(variable:&str) -> String {
-    general_purpose::STANDARD.decode(variable).unwrap().into_iter().map(|c| c as char).collect()
+    general_purpose::STANDARD_NO_PAD.decode(variable.as_bytes()).ok().map(|v| v.into_iter().map(|c| c as char).collect()).unwrap_or_else(|| String::new())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn test_encrypt() {
-        let variable = "test";
-        let key = None;
-        assert_eq!(encrypt(variable, key), "nuds");
+    fn test_encrypt_decrypt() {
+        let variable = "Hello, world!";
+        let key = "secret";
+        let encrypted = encrypt(variable, Some(key));
+        let decrypted = decrypt(&encrypted, Some(key));
+        assert_eq!(variable, decrypted);
     }
 
     #[test]
-    fn test_decrypt() {
-        let variable = "nuds";
-        let key = None;
-        assert_eq!(decrypt(variable, key), "test");
+    fn test_encrypt_different_chars() {
+        let variable = "!@#$%^&*()_+=/.,<>?;:'\"[]{}\\|`~";
+        let key = "secret";
+        let encrypted = encrypt(variable, Some(key));
+        let decrypted = decrypt(&encrypted, Some(key));
+        assert_eq!(variable, decrypted);
     }
 
     #[test]
-    fn test_base64_encode() {
-        let variable = "test";
-        assert_eq!(base64_encode(variable), "dGVzdA==");
+    fn test_base64_encode_decode() {
+        let variable = "Hello, world!";
+        let encoded = base64_encode(variable);
+        let decoded = base64_decode(&encoded);
+        assert_eq!(variable, decoded);
     }
 
     #[test]
-    fn test_base64_decode() {
-        let variable = "dGVzdA==";
-        assert_eq!(base64_decode(variable), "test");
+    fn test_base64_encode_decode_different_chars() {
+        let variable = "!@#$%^&*()_+=/.,<>?;:'\"[]{}\\|`~";
+        let encoded = base64_encode(variable);
+        let decoded = base64_decode(&encoded);
+        assert_eq!(variable, decoded);
     }
+
 }
 
